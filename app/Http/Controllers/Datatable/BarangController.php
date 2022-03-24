700a244a8aed6c4e56fd261aed37633bfd802cfa
rizky-bagus <rizky.bagus@gits.id>
2022-03-24 20:44:19 +0700

    feat: logs monitoring

--- app/Http/Controllers/Datatable/BarangController.php
+++ app/Http/Controllers/Datatable/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Datatable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\BarangService;
+use Illuminate\Support\Facades\Log;
 
 class BarangController extends Controller
 {
@@ -20,6 +21,12 @@ class BarangController extends Controller
      */
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
-        return $this->barangService->generateYajraDatatable();
+        try {
+            Log::debug('Connect To Datatable.');
+            return $this->barangService->generateYajraDatatable();
+        } catch (\Throwable $th) {
+            Log::error("fail Connect To Datatable.",$th);
+        }
+            
     }
 }
